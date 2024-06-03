ee74842e363ea858309f82e040540eee7077d4c6
hanichris <hanichris71@gmail.com>
2024-06-04 02:27:22 +0300

    Refactor views to simplify index url.

--- fetch/urls.py
+++ fetch/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', view=views.index, name='index'),
+    path('', view=views.IndexView.as_view(), name='index'),
     path('success/', views.success, name='success')
 ]
--- fetch/views.py
+++ fetch/views.py
@@ -1,26 +1,29 @@
-from django.shortcuts import get_object_or_404, render
-from django.http.request import HttpRequest
-from django.http.response import HttpResponseRedirect, JsonResponse
 from django.db.utils import IntegrityError
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseRedirect, JsonResponse, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.views import View
 
 from .models import Posts
 
 # Create your views here.
 
-def index(request: HttpRequest):
-    if request.method == 'GET':
+class IndexView(View):
+    def get(self, request, *args, **kwargs) -> HttpResponse:
         return render(request, 'fetch/index.html')
-    if request.method == "POST":
+    
+    def post(
+            self, request: HttpRequest, *args, **kwargs
+    ) -> HttpResponse | JsonResponse:
         import json
         data = json.loads(request.body.decode("utf-8"))
-        try:
-            Posts.objects.create(**data)
-        except IntegrityError:
-            return JsonResponse({
-                'dataError': True,
-                'id': data.get('id'),
-            })
-    return HttpResponseRedirect('success')
+        post, created = Posts.objects.get_or_create(**data)
+        if created:
+            return HttpResponseRedirect('success')
+        return JsonResponse({
+            'dataError': True,
+            'id': post.id, # type: ignore
+        })
 
 
 def success(request):
